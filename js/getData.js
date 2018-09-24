2561c0a85e79ac596ad716be2fe6042b4111a041
bloonsking <andreas.sanz@hotmail.com>
2018-09-24 16:49:36 +0200

    first commit

---
+++ js/getData.js
@@ -0,0 +1,58 @@
+var xhrMetar = new XMLHttpRequest();
+var xhrTaf = new XMLHttpRequest();
+var xhrNotam = new XMLHttpRequest();
+
+var searchButton = document.getElementById('search');
+
+
+
+getInfo = function (xhr, name, valueName, icaok, isNotam) {
+
+    document.getElementById(name + 'Text').innerHTML = "Söker...";
+    document.getElementById(name + "H").innerHTML = name.toUpperCase() + " - " +icaok ;
+
+    if(xhr.status === 200){
+
+        responeObject = JSON.parse(xhr.responseText);
+
+        var content = "";
+        if(!isNotam){
+        content = responeObject[valueName];
+        }
+        else{
+        content = responeObject[0][valueName];
+        }
+
+        document.getElementById(name + 'Text').innerHTML = content;
+    }
+    else{
+        document.getElementById(name + 'Text').innerHTML = "Ett fel inträffa. Försök igen.";
+    }
+};
+
+search = function (event) {
+    var icaok = document.getElementById('icao-k').value;
+    var metarUrl = 'https://avwx.rest/api/metar/' + icaok;
+    var tafUrl = 'https://avwx.rest/api/taf/' + icaok;
+    var notamURL = "https://v4p4sz5ijk.execute-api.us-east-1.amazonaws.com/anbdata/states/notams/notams-list?api_key=" +
+        config.key + "&format=json&type=&Qcode=&locations=ESOW&qstring=&states=&ICAOonly=";
+
+    if (icaok != "") {
+    getInfo(xhrMetar, 'metar', 'Raw-Report', icaok, false);
+    getInfo(xhrTaf, 'taf', 'Raw-Report', icaok, false);
+    getInfo(xhrNotam, 'notam', 'message', icaok, true);
+
+    xhrMetar.open('GET', metarUrl, true);
+    xhrMetar.send(null);
+
+    xhrTaf.open('GET', tafUrl, true);
+    xhrTaf.send(null);
+
+    xhrNotam.open('GET', notamURL, true);
+    xhrNotam.send(null);
+    }
+    event.preventDefault();
+}
+
+
+searchButton.addEventListener('click', search);

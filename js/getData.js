52f75d0cdb1030262afa113cabd03d1919c94cb1
Andreas Sanz Lerena <94andsan@mk.se>
2018-10-03 11:41:43 +0200

    Uppdatera design och js

--- js/getData.js
+++ js/getData.js
@@ -4,12 +4,14 @@ var xhrNotam = new XMLHttpRequest();
 
 var searchButton = document.getElementById('search');
 
-
+var icaok = document.getElementById('icao-k').value;
+var metarUrl = 'https://avwx.rest/api/metar/' + icaok;
+var tafUrl = 'https://avwx.rest/api/taf/' + icaok;
+var notamURL = "https://v4p4sz5ijk.execute-api.us-east-1.amazonaws.com/anbdata/states/notams/notams-list?api_key=" +
+    config.key + "&format=json&type=&Qcode=&locations=ESOW&qstring=&states=&ICAOonly=";
 
 getInfo = function (xhr, name, valueName, icaok, isNotam, isReload=false) {
-    document.getElementById(name + 'Text').innerHTML = "Söker...";
     document.getElementById(name + "H").innerHTML = name.toUpperCase() + " - " +icaok ;
-
     if(xhr.status === 200){
         responeObject = JSON.parse(xhr.responseText);
 
@@ -34,26 +36,35 @@ getInfo = function (xhr, name, valueName, icaok, isNotam, isReload=false) {
     }
 };
 
+xhrMetar.onload = function () {
+    getInfo(xhrMetar, 'metar', 'Raw-Report', icaok, false);
+}
+xhrTaf.onload = function () {
+    getInfo(xhrTaf, 'taf', 'Raw-Report', icaok, false);
+}
+xhrNotam.onload = function () {
+    getInfo(xhrNotam, 'notam', 'message', icaok, true);
+}
+
 search = function (event) {
-    var icaok = document.getElementById('icao-k').value;
-    var metarUrl = 'https://avwx.rest/api/metar/' + icaok;
-    var tafUrl = 'https://avwx.rest/api/taf/' + icaok;
-    var notamURL = "https://v4p4sz5ijk.execute-api.us-east-1.amazonaws.com/anbdata/states/notams/notams-list?api_key=" +
-        config.key + "&format=json&type=&Qcode=&locations=ESOW&qstring=&states=&ICAOonly=";
+    icaok = document.getElementById('icao-k').value;
+    metarUrl = 'https://avwx.rest/api/metar/' + icaok;
+    tafUrl = 'https://avwx.rest/api/taf/' + icaok;
+    notamURL = "https://v4p4sz5ijk.execute-api.us-east-1.amazonaws.com/anbdata/states/notams/notams-list?api_key=" +
+        config.key + "&format=json&type=&Qcode=&locations=" + icaok + "&qstring=&states=&ICAOonly=";
 
     if (icaok != "") {
-    getInfo(xhrMetar, 'metar', 'Raw-Report', icaok, false);
-    getInfo(xhrTaf, 'taf', 'Raw-Report', icaok, false);
-    getInfo(xhrNotam, 'notam', 'message', icaok, true);
-
     xhrMetar.open('GET', metarUrl, true);
     xhrMetar.send(null);
+    document.getElementById('metarText').innerHTML = "Söker...";
 
     xhrTaf.open('GET', tafUrl, true);
     xhrTaf.send(null);
+    document.getElementById('tafText').innerHTML = "Söker...";
 
     xhrNotam.open('GET', notamURL, true);
     xhrNotam.send(null);
+    document.getElementById('notamText').innerHTML = "Söker...";
     }
     event.preventDefault();
 }
